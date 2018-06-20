931acafba5fd42a6e4dfd1bce66e3e52490c1f2b
Даниил Северин <>
2018-06-21 01:10:16 +0300

    Some refactoring

--- client/views/Home.vue
+++ client/views/Home.vue
@@ -1,13 +1,8 @@
 <template>
   <div class="page">
      <Canvas v-bind:width="300" v-bind:height="500">
-    
         <Rectangle v-bind:left="100"  v-bind:fill="'red'" v-bind:height="10" />
- <Rectangle v-bind:left="100"  v-bind:fill="'green'" v-bind:height="100" v-bind:width="100" />
-
      </Canvas>
-    
-  
   </div>
 </template>
 
